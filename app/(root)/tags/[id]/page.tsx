b5a7e20009fe52152d3719ffc48dd5629b2653a3
sothulthorn <thornsothul@gmail.com>
2024-05-02 21:13:32 +0900

    Create Tags Details Page

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,59 @@
+import QuestionCard from '@/components/cards/QuestionCard';
+import NoResult from '@/components/shared/NoResult';
+import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
+import { getQuestionsByTagId } from '@/lib/actions/tag.action';
+import { URLProps } from '@/types';
+import React from 'react';
+
+const Page = async ({ params, searchParams }: URLProps) => {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: searchParams.page ? +searchParams.page : 1,
+    searchQuery: searchParams.q,
+  });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+
+      <div className="mt-11 w-full">
+        <LocalSearchbar
+          route={`/tags/${params.id}`}
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for tag question"
+          otherClasses="flex-1"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question: any) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no tag to show"
+            description="Be the first break the silience! Ask a Question and kickstart the
+          discussion. Our query could be the next bigh thing others learn from.
+          Get involved!"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Page;
